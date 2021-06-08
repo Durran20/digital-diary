ac9f6c1c3ef0bdc970eb0f1d646c18b5db82e9ee
Durran20 <durranfurtado2099@gmail.com>
2021-06-08 09:58:54 +0530

    Implement tailwind in project

--- update.php
+++ update.php
@@ -62,6 +62,7 @@
     
     <!-- Custom styles for this template -->
     <link href="styles.css" rel="stylesheet">
+    <link href="styles2.css" rel="stylesheet">
   </head>
   <body class="back-g">
     
@@ -129,6 +130,68 @@
         <a href="home.php"><button class="w-100 btn btn-primary btn-lg">Home</button></a>
       </div>
     </div>
+
+
+
+    <!-- CARD COMPONENT -->
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl mt-5">
+            <div class="flex-shrink-0">
+              <img src="https://i.ibb.co/vX8pYzZ/logo-only.png" alt="" class="h-12 w-12" />
+            </div>
+            <div class="ml-6 pt-1">
+              <h4 class="text-xl text-gray-900">Traversy Media</h4>
+              <p class="text-base text-gray-600">You have a new message!</p>
+            </div>
+          </div>
+      
+          <!-- CARD COLUMNS -->
+          <div class="container mt-5 mx-auto px-2">
+            <!-- Flex on med screens and up -->
+            <div class="md:flex">
+              <div
+                class="flex-1 text-gray-700 text-center bg-gray-400 px-5 py-5 m-2 rounded"
+              >
+                <div class="lg:flex lg:items-center">
+                  <div class="lg:flex-shrink-0">
+                    <img class="rounded-lg lg:w-64" src="img/camaro5.jpg" alt="" />
+                  </div>
+                  <div class="mt-4 lg:mt-0 lg:ml-6">
+                    <div
+                      class="uppercase tracking-wide text-sm text-indigo-600 font-bold"
+                    >
+                      Networking
+                    </div>
+                    <a
+                      href="#"
+                      class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+                      >Finding connections to help your business grow</a
+                    >
+                  </div>
+                </div>
+              </div>
+              <div
+                class="flex-1 text-gray-700 text-center bg-gray-400 px-5 py-5 m-2 rounded"
+              >
+                <div class="lg:flex lg:items-center">
+                  <div class="lg:flex-shrink-0">
+                    <img class="rounded-lg lg:w-64" src="img/camaro6.jpg" alt="" />
+                  </div>
+                  <div class="mt-4 lg:mt-0 lg:ml-6">
+                    <div
+                      class="uppercase tracking-wide text-sm text-indigo-600 font-bold"
+                    >
+                      Marketing
+                    </div>
+                    <a
+                      href="#"
+                      class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+                      >Finding customers for your new business</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> 
   </main>
 
   <footer class="my-5 pt-5 text-muted text-center text-small">
